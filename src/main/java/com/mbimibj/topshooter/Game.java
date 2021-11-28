754af4d18fffe0186097ab2cf0a5dd0e3909f49e
mbimbij <joseph.mbimbi@gmail.com>
2021-11-28 10:33:57 +0100

    Initial Commit

---
+++ src/main/java/com/mbimibj/topshooter/Game.java
@@ -0,0 +1,4 @@
+package com.mbimibj.topshooter;
+
+public class Game {
+}
---
+++ src/main/java/com/mbimibj/topshooter/Player.java
@@ -0,0 +1,11 @@
+package com.mbimibj.topshooter;
+
+public class Player {
+    private final double x;
+    private final double y;
+
+    public Player(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
